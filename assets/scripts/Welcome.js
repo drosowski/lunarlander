7057734887392b5187e8a9f2c4cbc383231aa6f7
Daniel Rosowski <rosowski@smartsquare.de>
2017-07-16 07:34:47 +0200

    Added welcome screen

---
+++ assets/scripts/Welcome.js
@@ -0,0 +1,34 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //    default: null,      // The default value will be used only when the component attaching
+        //                           to a node for the first time
+        //    url: cc.Texture2D,  // optional, default is typeof default
+        //    serializable: true, // optional, default is true
+        //    visible: true,      // optional, default is true
+        //    displayName: 'Foo', // optional
+        //    readonly: false,    // optional, default is false
+        // },
+        // ...
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        var self = this;
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            // When there is a key being pressed down, judge if it's the designated directional button and set up acceleration in the corresponding direction
+            onKeyPressed: function(keyCode, event) {
+                cc.log("loading game scene");
+                cc.director.loadScene("game");
+            }
+        }, self.node);
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
